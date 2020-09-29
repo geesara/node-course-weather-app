2e538c5b2cab5ad040eb19b3dc72de515182bc45
Sahan Geesara <sgeesara@gmail.com>
2020-09-29 12:40:22 +0530

    Add humidity data to forcast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,7 @@ const forecast = (longititude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humiduty is ' + body.current.humidity + '.')
         }
     })
 }
